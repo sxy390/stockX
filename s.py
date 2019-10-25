f2b27ee6fd785dac14904a9310687a90e88a3bd6
shuo <sxy390@case.edu>
2019-10-25 01:21:27 -0400

    insert symbols

--- s.py
+++ s.py
@@ -1,33 +1,74 @@
 """Stocks CLI
 Usage:
     s.py
-    s.py <name>
-    s.py -h|--help
+    s.py -a <symbol>
     s.py -d|--dummy
+    s.py -h|--help
     s.py -v|--version
+    s.py -w|--watchlist
 Options:
-    <name>  Optional name argument.
-    -h --help  Show this screen.
+    <symbol> Optional symbol.
+    -a --add  Add symbol to watchlist.
     -d --dummy  Show something dummy.
+    -h --help  Show this screen.
     -v --version  Show version.
+    -w --watchlist  Show symbols in current watchlist.
 """
 
 from docopt import docopt
+import mysql.connector
 
 def say_hello(name):
     return("Hello {}!".format(name))
 
 class stocks:
-    def dummy():
+    def __init__(self):
+        self.cnx = mysql.connector.connect(
+            user='testuser',
+            password='test123test!',
+            host='localhost',
+            database='stock'
+        )
+        self.cursor = self.cnx.cursor()
+        self.watchlist = []
+
+    def dummy(self):
         print("Hello, welcome!")
+
+    def addToWatch(self, symbol):
+        add_symbol = "INSERT INTO symbols (symbol) VALUES (%(symbol)s)"
+        data_symbol = {'symbol': symbol}
+        self.cursor.execute(add_symbol, data_symbol)
+        self.cnx.commit()
+        print("{} is added to the watchlist.".format(symbol))
+
+    def getWatchlist(self):
+        self.cursor.execute("SELECT * FROM symbols")
+        result = self.cursor.fetchall()
+        self.watchlist = [s[0] for s in result]
+
+    def printWatchlist(self):
+        for s in self.watchlist:
+            print(s)
+        print("----------------------------")
+        print("{} symbols in the watchlist.".format(len(self.watchlist)))
+
+    def exit(self):
+        self.cursor.close()
+        self.cnx.close()
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='1.0')
-    if arguments['<name>']:
-        print(say_hello(arguments['<name>']))
-        print(arguments)
-    elif arguments["--dummy"]:
+    stocks = stocks()
+    if arguments["--dummy"]:
         stocks.dummy()
+    elif arguments["--add"]:
+        stocks.addToWatch(arguments['<symbol>'])
+        stocks.exit()
+    elif arguments["--watchlist"]:
+        stocks.getWatchlist()
+        stocks.printWatchlist()
+        stocks.exit()
     else:
         print(arguments)
